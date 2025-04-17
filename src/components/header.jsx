241529cfed5d9aa662231c8e0cf350547e7db9bb
Harsha Anuga <harshaanuga358@gmail.com>
2025-04-17 22:54:32 +0530

    added header component

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    const[categories,setcategories]=useState([]);
+    const[showCategories,setShowCatogories]=useState([false]);
+
+  return (
+    <nav>
+    <div>
+        <Link to="/" className="w-8" alt=""/>
+        
+
+    </div>
+    </nav>
+  )
+}
+
+export default Header;
